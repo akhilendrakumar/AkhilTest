c2c8ad59c2989b2c10bcebf628929b07f9992f45
HP <HP@HOME>
2019-09-05 20:08:34 +0530

    MavenDemo code pushed

---
+++ mavenDemo/src/test/java/conm/mavenDemo/TestMaven.java
@@ -0,0 +1,18 @@
+package conm.mavenDemo;
+
+import org.testng.annotations.Test;
+
+public class TestMaven {
+
+@Test
+public void test1() {
+	System.out.println("in test1 of TestMaven");
+}
+
+@Test
+public void test2() {
+	System.out.println("in test2 TestMaven");
+}
+
+
+}
---
+++ mavenDemo/src/test/java/conm/mavenDemo/TestMaven2.java
@@ -0,0 +1,15 @@
+package conm.mavenDemo;
+
+import org.testng.annotations.Test;
+
+public class TestMaven2 {
+	@Test
+	public void test1() {
+		System.out.println("in test1 of TestMaven2 ");
+	}
+	@Test
+	public void test2() {
+		System.out.println("in test2 of TestMaven2 ");
+	}
+
+}
